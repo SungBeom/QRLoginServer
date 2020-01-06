6b3d789a67fd283c1308b0d24689e89a0c495b2e
SungBeom <psb_94@naver.com>
2020-01-06 14:47:25 +0900

    Add koa-bodyparser library

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
+const router = new Router();
 const api = require('./api');
 
-app.use(api.routes()).use(api.allowedMethods());
+router.use(api.routes());
+
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(3000, () => {
     console.log('server is listening to port 3000');
