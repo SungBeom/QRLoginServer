0664bd56ac9e83ae8f6e47044ce71274686221ba
SungBeom <psb_94@naver.com>
2020-01-06 16:29:29 +0900

    Add sign up API

--- src/api/index.js
+++ src/api/index.js
@@ -14,6 +14,20 @@ api.get('/', (ctx, next) => {
     ctx.body = "Connected";
 });
 
+api.post('/signup', async (ctx, next) => {
+    const { uId, uPw, uName, uEngName } = ctx.request.body;
+
+    await model.sequelize.models.Users.create({
+        userId: uId, userPw: uPw, name: uName, engName: uEngName
+    }).then(() => {
+        ctx.status = 200;
+    }).catch(err => {
+        console.log(err);
+        console.log(ctx.body = "회원가입이 불가능한 정보입니다.[userID 중복, 타입 불일치, 정보 누락]");
+        ctx.status = 500;
+    });
+});
+
 api.get('/users', (ctx, next) => {
     console.log(ctx.body = "How to Use: [GET]/users/:userId\nSearch for user with id 'userId'");
 });
@@ -32,7 +46,6 @@ api.get('/users/:uId', async (ctx, next) => {
             console.log("Search Failed");
             ctx.body = "해당 ID를 가진 유저는 존재하지 않습니다.";
         }
-
         ctx.status = 200;
     }).catch(err => {
         console.log(err);
