c6c4aa1434db25dc15a5ac1fd55fc91650d168a6
SungBeom <psb_94@naver.com>
2020-01-07 17:08:37 +0900

    Test cookie creation and deletion

--- src/api/index.js
+++ src/api/index.js
@@ -14,6 +14,21 @@ api.get('/', (ctx, next) => {
     ctx.body = "Connected";
 });
 
+// 쿠키 생성 테스트 API
+api.get('/cookies', (ctx, next) => {
+    // 하루동안 유지되는 쿠키 생성(Name: name, Value: value)
+    ctx.cookies.set('name', 'value', { httpOnly: false, maxAge: 1000 * 60 * 60 * 24 * 1 });
+    ctx.body = "Cookies test";
+    ctx.status = 200;
+});
+
+// 쿠키 삭제 테스트 API
+api.delete('/cookies', (ctx, next) => {
+    ctx.cookies.set('name', '');
+    ctx.body = "Delete cookies";
+    ctx.status = 204;
+});
+
 // 회원 가입 API
 // req: uId(신규 유저 Id/string), uPw(신규 우저 Pw/string), uName(신규 유저 이름/string), uEngName(신규 유저 영어 이름/string)
 // res: 성공 - OK / 실패 - Fail message / 에러 - Error message
