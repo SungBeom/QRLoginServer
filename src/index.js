97afbb5106e43b9909b058fe27f52a1645b58de1
SungBeom <psb_94@naver.com>
2020-01-08 10:26:06 +0900

    Test counting session access

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const session = require('koa-session');
 
 const app = new Koa();
+app.keys = ['secret key'];
 const router = new Router();
 const api = require('./api');
 
@@ -11,6 +13,15 @@ router.use(api.routes());
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.use(session(app));
+app.use(ctx => {
+    let n = ctx.session.views || 0;
+    ctx.session.views = ++n;
+
+    if(n === 1) ctx.body = 'Welcome here for the first time!';
+    else ctx.body = "You've visited this page " + n + " times!";
+    console.log(n);
+})
 
 app.listen(3000, () => {
     console.log('server is listening to port 3000');
