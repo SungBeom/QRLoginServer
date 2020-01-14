4828916ce877a329fb170fc2fd0ad1843ada1885
SungBeom <psb_94@naver.com>
2020-01-14 10:35:32 +0900

    Test qr code with browserify

--- src/qrcode/index.js
+++ src/qrcode/index.js
@@ -1,9 +1,6 @@
 const QRCode = require('qrcode');
+const canvas = document.getElementById('canvas');
 
-// terminal test
-QRCode.toString('crpyted cookie info which never decode when who dont have key', { type: 'terminal' }, (err, url) => {
+QRCode.toCanvas(canvas, "localhost:3000/auth", { width: 300, color: { dark: "#222222FF", light: "#F0F8FFFF" } }, err => {
     if(err) console.log(err);
-    else console.log(url);
 });
-
-module.exports = QRCode;
