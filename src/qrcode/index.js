faaffd3fe4be550c2cdc1fc90d5b7440ce2653d4
SungBeom <psb_94@naver.com>
2020-01-14 15:18:24 +0900

    Generate random token

--- src/qrcode/index.js
+++ src/qrcode/index.js
@@ -1,6 +1,14 @@
 const QRCode = require('qrcode');
 const canvas = document.getElementById('canvas');
 
-QRCode.toCanvas(canvas, "localhost:3000/auth", { width: 300, color: { dark: "#222222FF", light: "#F0F8FFFF" } }, err => {
+let QRContent;
+fetch("http://localhost:3000/tokens")
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    QRContent = data;
+});
+
+QRCode.toCanvas(canvas, "http://localhost:3000/auth/" + QRContent, { width: 300, color: { dark: "#222222FF", light: "#F0F8FFFF" } }, err => {
     if(err) console.log(err);
 });
