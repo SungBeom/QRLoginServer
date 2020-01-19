d478addf5a57e2bb6c8c4d625c966359b024ea38
SungBeom <psb_94@naver.com>
2020-01-19 13:17:14 +0900

    [KAKAOACCNT-2523]Fix API call to refactored API

--- src/qrcode/index.js
+++ src/qrcode/index.js
@@ -1,26 +1,26 @@
 const QRCode = require('qrcode');
 const canvas = document.getElementById('canvas');
 
-fetch("http://localhost:3000/tokens")
+fetch("http://localhost:3000/codes", { method: 'post' })
 .then(res => res.json())
 .then(QRContent => {
-    QRCode.toCanvas(canvas, "http://localhost:3000/auth/" + QRContent.randomToken, { width: 300, color: { dark: "#222222FF", light: "#F0F8FFFF" } }, err => {
+    QRCode.toCanvas(canvas, "http://localhost:3000/codes/" + QRContent.randomCode, { width: 300, color: { dark: "#222222FF", light: "#F0F8FFFF" } }, err => {
         if(err) console.log(err);
 
         let count = 0;
         let timer = setInterval(() => {
             if(++count === 30) {
-                fetch("http://localhost:3000/tokens/" + QRContent.randomToken, { method: 'delete' })
+                fetch("http://localhost:3000/codes/" + QRContent.randomCode)
                 .then(() => {
                     clearInterval(timer);
                 });
             }
 
-            fetch("http://localhost:3000/tokens/" + QRContent.randomToken)
+            fetch("http://localhost:3000/codes/" + QRContent.randomCode)
             .then(res2 => res2.json())
             .then(result => {
                 if(result.loginId !== null) {
-                    fetch("http://localhost:3000/tokens/" + QRContent.randomToken, { method: 'delete' })
+                    fetch("http://localhost:3000/codes/" + QRContent.randomCode, { method: 'delete' })
                     .then(() => {
                         location.href = "success.html?uId=" + result.loginId;
                     });
@@ -29,3 +29,4 @@ fetch("http://localhost:3000/tokens")
         }, 1000);
     });
 });
+
