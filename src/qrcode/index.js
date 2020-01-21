ebc7c3a92f1b21bd899a6777b392ae0be991f051
SungBeom <psb_94@naver.com>
2020-01-21 14:30:52 +0900

    [KAKAOACCNT-2505]Update to match changed data name

--- src/qrcode/index.js
+++ src/qrcode/index.js
@@ -4,29 +4,31 @@ const canvas = document.getElementById('canvas');
 fetch("http://localhost:3000/codes", { method: 'post' })
 .then(res => res.json())
 .then(QRContent => {
-    QRCode.toCanvas(canvas, "http://localhost:3000/codes/" + QRContent.randomCode, { width: 300, color: { dark: "#222222FF", light: "#F0F8FFFF" } }, err => {
+    QRCode.toCanvas(canvas, "http://localhost:3000/codes/" + QRContent.rcodeData, { width: 300, color: { dark: "#222222FF", light: "#F0F8FFFF" } }, err => {
         if(err) console.log(err);
 
         let count = 0;
         let timer = setInterval(() => {
             if(++count === 30) {
-                fetch("http://localhost:3000/codes/" + QRContent.randomCode)
+                fetch("http://localhost:3000/codes/" + QRContent.codeData)
                 .then(() => {
-                    clearInterval(timer);
+                    fetch("http://localhost:3000/codes/" + QRContent.codeData, { method: 'delete' })
+                    .then(() => {
+                        clearInterval(timer);
+                    });
                 });
             }
 
-            fetch("http://localhost:3000/codes/" + QRContent.randomCode, { method: 'put' })
+            fetch("http://localhost:3000/codes/" + QRContent.codeData, { method: 'put' })
             .then(res2 => res2.json())
             .then(result => {
-                if(result.loginId !== null) {
-                    fetch("http://localhost:3000/codes/" + QRContent.randomCode, { method: 'delete' })
+                if(result.userId !== null) {
+                    fetch("http://localhost:3000/codes/" + QRContent.codeData, { method: 'delete' })
                     .then(() => {
-                        location.href = "success.html?uId=" + result.loginId;
+                        location.href = "success.html?uId=" + result.userId;
                     });
                 }
             });
         }, 1000);
     });
 });
-
