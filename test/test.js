550aef3ac1d1b4c82e60213123cc4fc9137f1601
SungBeom <psb_94@naver.com>
2020-02-10 15:38:41 +0900

    [KAKAOACCNT-2581]Add sign up and login test code

---
+++ test/test.js
@@ -0,0 +1,176 @@
+const request = require('request');
+const expect = require('chai').use(require('chai-http')).expect;
+require('dotenv').config();
+
+const userId = "tId";
+const userPw = "tPw";
+const name = "tName";
+const engName = "tEngName";
+
+/*
+ * ID 중복 확인(없는 유저)
+ */
+describe("GET " + process.env.TEST_API + "/users/ids/" + userId, () => {
+    it('200', done => {
+        request.get(process.env.TEST_API + "/users/ids/" + userId, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            expect(body).to.equal('0');
+            done();
+        });
+    });
+});
+
+/*
+ * 회원 가입
+ */
+describe("POST " + process.env.TEST_API + "/users", () => {
+    it('200', done => {
+        const options = {
+            url: process.env.TEST_API + "/users",
+            headers: {
+                'referer': process.env.TEST_SIGNUP_REFERER
+            },
+            body: {
+                'userId': userId,
+                'userPw': userPw,
+                'name': name,
+                'engName': engName
+            },
+            json: true
+        }
+
+        request.post(options, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            done();
+        });
+    });
+});
+
+/*
+ * ID 중복 조회(있는 유저)
+ */
+describe("GET " + process.env.TEST_API + "/users/ids/" + userId, () => {
+    it('200', done => {
+        request.get(process.env.TEST_API + "/users/ids/" + userId, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            expect(body).to.equal('1');
+            done();
+        });
+    });
+});
+
+/*
+ * 유저 정보 조회(로그인 전)
+ */
+describe("GET " + process.env.TEST_API + "/users", () => {
+    it('401', done => {
+        request.get(process.env.TEST_API + "/users", (err, res, body) => {
+            expect(res.statusCode).to.equal(401);
+            expect(body).to.equal('You need to login.');
+            done();
+        });
+    });
+});
+
+/*
+ * 일반(ID, 비밀번호) 로그인 실패
+ */
+describe("POST " + process.env.TEST_API + "/auth", () => {
+    it('401', done => {
+        const options = {
+            url: process.env.TEST_API + "/auth",
+            headers: {
+                'referer': process.env.TEST_LOGIN_REFERER
+            },
+            body: {
+                'userId': userId,
+                'userPw': "tt",
+                'codeData': "",
+                'kakaoToken': ""
+            },
+            json: true
+        }
+
+        request.post(options, (err, res, body) => {
+            expect(res.statusCode).to.equal(401);
+            expect(body).to.equal('The ID or password is incorrect.');
+            done();
+        });
+    });
+});
+
+let cookie;
+
+/*
+ * 일반(ID, 비밀번호) 로그인 성공
+ */
+describe("POST " + process.env.TEST_API + "/auth", () => {
+    it('200', done => {
+        const options = {
+            url: process.env.TEST_API + "/auth",
+            headers: {
+                'referer': process.env.TEST_LOGIN_REFERER
+            },
+            body: {
+                'userId': userId,
+                'userPw': userPw,
+                'codeData': "",
+                'kakaoToken': ""
+            },
+            json: true
+        }
+
+        request.post(options, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            expect(res).to.cookie('accessToken');
+            expect(res).to.cookie('accessToken.sig');
+            cookie = res.headers['set-cookie'];
+            done();
+        });
+    });
+});
+
+/*
+ * 유저 정보 조회(로그인 후)
+ */
+describe("GET " + process.env.TEST_API + "/users", () => {
+    it('200', done => {
+        const options = {
+            url: process.env.TEST_API + "/users",
+            headers: {
+                'Cookie': cookie
+            }
+        }
+
+        const result = {
+            'userId': userId,
+            'name': name,
+            'engName': engName
+        }
+
+        request.get(options, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            expect(body).to.equal(JSON.stringify(result));
+            done();
+        });
+    });
+});
+
+/*
+ * 회원 탈퇴
+ */
+describe("DELETE " + process.env.TEST_API + "/users", () => {
+    it('200', done => {
+        const options = {
+            url: process.env.TEST_API + "/users",
+            headers: {
+                'Cookie': cookie
+            }
+        }
+
+        request.delete(options, (err, res, body) => {
+            expect(res.statusCode).to.equal(200);
+            done();
+        });
+    });
+});
